041a1fcc94a14f96bc13aaa2aa61f5a75344ff68
rivajuniorfr <rivajunior.rec@gmail.com>
2022-02-14 21:26:24 -0300

    Flipproject

---
+++ aula-js/for-in.js
@@ -0,0 +1,11 @@
+let pessoa = {
+    nome: "Felipe",
+    idade:"38",
+    sexo: "masc",
+    peso: "74"
+    }
+    
+    for (let p in pessoa) {
+        console.log('dados da pessoa', p)
+        console.log('dados da pessoa', pessoa[p])
+}
---
+++ aula-js/forsimples.js
@@ -0,0 +1,5 @@
+const notas = [10,5,6,7]
+
+for (let i = 0; i < notas.length; i++) {
+    console.log ('indice', i, 'valor', notas[i])
+}
