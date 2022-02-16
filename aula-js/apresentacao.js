7f9ed8553db1c813505d1b0af35fc5587dbbb070
rivajuniorfr <rivajunior.rec@gmail.com>
2022-02-16 20:52:32 -0300

    portfolio

---
+++ aula-js/apresentacao.js
@@ -0,0 +1,11 @@
+const escola = "S3nac"
+console.log(escola.charAt(1))
+console.log(escola[1])
+console.log(escola.charAt(7))
+console.log(escola.indexOf('3'))
+console.log(escola.substring(0))
+console.log(escola.substring(0,2))
+console.log(escola.replace('3','e'))
+var cores = 'rosa roxo laranja verde azul'
+var resultado = cores.split(' ')[1].replace(/o/g, '0')  //converte em array
+console.log(resultado)
